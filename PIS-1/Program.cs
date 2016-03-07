4bc116f73d303680eaf3ac6d64e0092e5871fb9b
Imarun00 <imarun00@fesb.hr>
2016-03-07 10:21:40 +0100

    Update Program.cs
    Fixed message

--- PIS-1/Program.cs
+++ PIS-1/Program.cs
@@ -10,7 +10,7 @@ namespace PIS_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Jesus");
         }
     }
 }
